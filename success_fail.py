3e72510175a5323bcf7290ca9dfa89230f74cafa
Refan1 <reefan777k1@gmail.com>
2026-09-15 20:49:04 +0300

    Add files via upload

---
+++ success_fail.py
@@ -0,0 +1,13 @@
+
+grade =int(input("Enter your grade:"))
+
+if grade >=90:
+    print("Excellent!")
+elif grade >=80:
+    print("very good!")
+elif grade >=70:
+    print("good!")
+elif grade >=60:
+    print("pass!")
+else:
+    print("fail!")
